a7c881eaa551fe981ecfde1532f7b84bc7adf457
yeaby <adrian.copta@isa.utm.md>
2025-05-11 18:05:47 +0300

    configured project

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld  from '../components/HelloWorld.vue'
+import HomeView  from '../views/HomeView.vue'
 
 const routes = [
-  { path: '/', component: HelloWorld },
+  { path: '/', component: HomeView },
 ];
 
 export const router = createRouter({
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-6 bg-red-800 flex-1/2">
+        Home
+    </div>
+</template>
